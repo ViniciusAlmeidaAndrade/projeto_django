494b120c3348aeed83fd12d80e7fa8d8eef45b9f
vinícius Almeida Andrade <vini3104@hotmail.com>
2024-10-23 01:43:29 -0300

    mais coisa

--- produtos/models.py
+++ produtos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Relatorio_vist(models.Model):
+    nom_tecnicos = models.CharField(max_length=50)
+    nom_clientes =  models.CharField(max_length=50)
+    enderecos = models.CharField(max_length=150)
+    datas = models.DateField((""), auto_now=False, auto_now_add=True)
+    prod_usados =  models.CharField(max_length=255)
+    observacoes =  models.CharField(max_length=255)
+    
+    def _str_(self) -> str:
+        return self.nome
--- produtos/views.py
+++ produtos/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from.models import Relatorio_vist
 
 def relatorio_vist(request):
     if request.method == "GET":
@@ -7,7 +8,7 @@ def relatorio_vist(request):
         return render(request, 'relatorio_vist.html')
     elif request.method == "POST":
         n_tecnico= request.POST.get("n_tecnico")
-        n_cliente=request.Post.get("n_cliente")
+        n_cliente=request.POST.get("n_cliente")
         print(n_tecnico)
         return HttpResponse("Estou no inserir")
 def inserir_produto(request):
